a68ab87eff2a86a1af08c173d7bef62a47f4eea9
emirsakarcan15 <emirsakarcanau@gmail.com>
2026-04-30 20:19:47 +0300

    obtained TMDB API

--- front-end/src/pages/Recommendations.tsx
+++ front-end/src/pages/Recommendations.tsx
@@ -1,6 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { toast } from 'sonner';
 
 function Recommendations() {
+
+  const fetchRecommendations = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/recommend", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+      }
+      else {
+        toast.error("Failed to fetch recommendations. Please try again.");
+      }
+    } catch (error) {
+      toast.error("An error occurred while fetching recommendations. Please try again.");
+    }
+
+  useEffect(() => {
+    fetchRecommendations();
+  }, []);
+}
+
+  
+
+
+
   return (
     <div>Recommendations</div>
   )
